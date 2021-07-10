1d483e5839c660ed9482da435a567637e3a3fc61
Robert Vári <mail.robertvari@gmail.com>
2021-07-10 15:55:42 +0200

    update

--- 05_methods.py
+++ 05_methods.py
@@ -6,6 +6,9 @@ class CoffeeMachine:
         self.model = model
         self.turned_on = False
 
+        self.water = None
+        self.beans = None
+
     def turn_on(self):
         self.turned_on = True
 
@@ -17,11 +20,41 @@ class CoffeeMachine:
 
         print(f"{self.model} Wellcome. Do you want a cup of coffee?")
 
+    def fill_up_water(self):
+        print("Filling up water tank...")
+        time.sleep(2)
+        self.water = True
+
+    def fill_up_beans(self):
+        print("Filling up beans...")
+        time.sleep(2)
+        self.beans = True
+
     def make_coffee(self):
         assert self.turned_on, "First turn it on!"
+        assert self.water, "Please fill up water tank."
+        assert self.beans, "Please fill up coffee beans"
+
         print("Making coffee....")
+
+    def turn_off(self):
+        assert self.turned_on, "Machine is turned off."
+
+        self.turned_on = False
+        print(f"{self.model} Turning off...")
+        time.sleep(3)
+        print("See you next time!")
+
 
 
 my_coffee_machine = CoffeeMachine("DeLonghi")
+
 my_coffee_machine.turn_on()
+
+my_coffee_machine.fill_up_water()
+
+my_coffee_machine.fill_up_beans()
+
 my_coffee_machine.make_coffee()
+
+my_coffee_machine.turn_off()
