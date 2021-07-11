79ced8b1bdab9cf7b8e51cc15aceb24904817ba4
Robert Vári <mail.robertvari@gmail.com>
2021-07-11 14:15:39 +0200

    init

--- Blackjack/game_assets/assets.py
+++ Blackjack/game_assets/assets.py
@@ -6,11 +6,11 @@ class PlayerBase:
         self.in_game = True
 
 
-class Player(PlayerBase):
+class HumanPlayer(PlayerBase):
     pass
 
 
-class VirtualPlayer(PlayerBase):
+class AiPlayer(PlayerBase):
     pass
 
 
@@ -19,4 +19,17 @@ class Deck:
 
 
 class Card:
-    pass
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return f"Name: {self.name} Value: {self.value}"
+
+
+if __name__ == '__main__':
+    card1 = Card("Heart 2", 2)
+    card2 = Card("Heart 3", 3)
+    card3 = Card("Heart 4", 4)
+    hand = [card1, card2, card1]
+    print(hand)
