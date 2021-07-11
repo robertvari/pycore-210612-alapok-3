9ffae89fe208f02eb129877b18fbe2bfb9697fe6
Robert Vári <mail.robertvari@gmail.com>
2021-07-11 17:39:11 +0200

    battle of classes init

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,5 +1,6 @@
 import random
 
+
 class CharacterBase:
     races = {
         "human": {"strength": 50, "max_HP": 100},
@@ -19,6 +20,27 @@ class CharacterBase:
         self._max_HP = 0
         self._current_HP = 0
 
+    def create(self):
+        FIRST = ['A', 'Ag', 'Ar', 'Ara', 'Anu', 'Bal', 'Bil', 'Boro', 'Bern', 'Bra', 'Cas', 'Cere', 'Co', 'Con',
+                 'Cor', 'Dag', 'Doo', 'Elen', 'El', 'En', 'Eo', 'Faf', 'Fan', 'Fara', 'Fre', 'Fro', 'Ga', 'Gala', 'Has',
+                 'He', 'Heim', 'Ho', 'Isil', 'In', 'Ini', 'Is', 'Ka', 'Kuo', 'Lance', 'Lo', 'Ma', 'Mag', 'Mi', 'Mo',
+                 'Moon', 'Mor', 'Mora', 'Nin', 'O', 'Obi', 'Og', 'Pelli', 'Por', 'Ran', 'Rud', 'Sam', 'She', 'Sheel',
+                 'Shin', 'Shog', 'Son', 'Sur', 'Theo', 'Tho', 'Tris', 'U', 'Uh', 'Ul', 'Vap', 'Vish', 'Ya', 'Yo', 'Yyr']
+
+        SECOND = ['ba', 'bis', 'bo', 'bus', 'da', 'dal', 'dagz', 'den', 'di', 'dil', 'din', 'do', 'dor', 'dra',
+                  'dur', 'gi', 'gauble', 'gen', 'glum', 'go', 'gorn', 'goth', 'had', 'hard', 'is', 'ki', 'koon', 'ku',
+                  'lad', 'ler', 'li', 'lot', 'ma', 'man', 'mir', 'mus', 'nan', 'ni', 'nor', 'nu', 'pian', 'ra', 'rak',
+                  'ric', 'rin', 'rum', 'rus', 'rut', 'sek', 'sha', 'thos', 'thur', 'toa', 'tu', 'tur', 'tred', 'varl',
+                  'wain', 'wan', 'win', 'wise', 'ya']
+
+        self._name = f"{random.choice(FIRST)}{random.choice(SECOND)}"
+
+
+        return self
+
+    def __repr__(self):
+        return self._name
+
 
 class Player(CharacterBase):
     pass
@@ -33,4 +55,6 @@ class NPC(CharacterBase):
 
 
 if __name__ == '__main__':
-    print(CharacterBase.races["name"])
+    for _ in range(100):
+        enemy = Enemy().create()
+        print(enemy)
