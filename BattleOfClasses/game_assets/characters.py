d242370b99cfad218af0bfd935bd8fa70447d441
Robert Vári <mail.robertvari@gmail.com>
2021-07-11 18:13:41 +0200

    battle of classes init

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,4 +1,5 @@
 import random
+from game_assets.utilities import get_fantasy_name
 
 
 class CharacterBase:
@@ -21,19 +22,7 @@ class CharacterBase:
         self._current_HP = 0
 
     def create(self):
-        FIRST = ['A', 'Ag', 'Ar', 'Ara', 'Anu', 'Bal', 'Bil', 'Boro', 'Bern', 'Bra', 'Cas', 'Cere', 'Co', 'Con',
-                 'Cor', 'Dag', 'Doo', 'Elen', 'El', 'En', 'Eo', 'Faf', 'Fan', 'Fara', 'Fre', 'Fro', 'Ga', 'Gala', 'Has',
-                 'He', 'Heim', 'Ho', 'Isil', 'In', 'Ini', 'Is', 'Ka', 'Kuo', 'Lance', 'Lo', 'Ma', 'Mag', 'Mi', 'Mo',
-                 'Moon', 'Mor', 'Mora', 'Nin', 'O', 'Obi', 'Og', 'Pelli', 'Por', 'Ran', 'Rud', 'Sam', 'She', 'Sheel',
-                 'Shin', 'Shog', 'Son', 'Sur', 'Theo', 'Tho', 'Tris', 'U', 'Uh', 'Ul', 'Vap', 'Vish', 'Ya', 'Yo', 'Yyr']
-
-        SECOND = ['ba', 'bis', 'bo', 'bus', 'da', 'dal', 'dagz', 'den', 'di', 'dil', 'din', 'do', 'dor', 'dra',
-                  'dur', 'gi', 'gauble', 'gen', 'glum', 'go', 'gorn', 'goth', 'had', 'hard', 'is', 'ki', 'koon', 'ku',
-                  'lad', 'ler', 'li', 'lot', 'ma', 'man', 'mir', 'mus', 'nan', 'ni', 'nor', 'nu', 'pian', 'ra', 'rak',
-                  'ric', 'rin', 'rum', 'rus', 'rut', 'sek', 'sha', 'thos', 'thur', 'toa', 'tu', 'tur', 'tred', 'varl',
-                  'wain', 'wan', 'win', 'wise', 'ya']
-
-        self._name = f"{random.choice(FIRST)}{random.choice(SECOND)}"
+        self._name = get_fantasy_name()
 
         self._race = random.choice(list(self.races))
         self._strength = self.races[self._race]["strength"]
--- BattleOfClasses/game_assets/items.py
+++ BattleOfClasses/game_assets/items.py
@@ -1,14 +1,62 @@
+import random
+
+from game_assets.utilities import get_fantasy_name
+
+
 class ItemBase:
-    pass
+    def __init__(self):
+        self._name = None
+        self._price = 0
+        self._weight = 0
+
+    def create(self):
+        self._name = get_fantasy_name()
+        self._price = random.randint(1, 100)
+        self._weight = random.randint(1, 100)
+        return self
+
+    def report(self):
+        print(f"Name: {self._name}")
+        print(f"Price: {self._price}")
+        print(f"Weight: {self._weight}")
 
 
 class Common(ItemBase):
-    pass
+    def create(self):
+        super().create()
+        self._price = random.randint(1, 10)
+        return self
 
 
 class Weapon(ItemBase):
-    pass
+    def create(self):
+        super().create()
+        self._price = random.randint(100, 10000)
+        return self
+
+
+class Magic(ItemBase):
+    def create(self):
+        super().create()
+        self._price = random.randint(500, 10000)
+        return self
 
 
 class Food(ItemBase):
-    pass
+    def create(self):
+        super().create()
+        self._weight = random.randint(1, 10)
+        self._price = random.randint(1, 10)
+
+        return self
+
+
+if __name__ == '__main__':
+    common_item = Common().create()
+    common_item.report()
+
+    weapon_item = Weapon().create()
+    weapon_item.report()
+
+    magic_item = Magic().create()
+    magic_item.report()
