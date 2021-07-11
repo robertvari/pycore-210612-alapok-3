fbfd9ad78981e72090273d03e644dee46857eba6
Robert Vári <mail.robertvari@gmail.com>
2021-07-11 17:31:00 +0200

    battle of classes init

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,3 +1,5 @@
+import random
+
 class CharacterBase:
     races = {
         "human": {"strength": 50, "max_HP": 100},
@@ -5,6 +7,17 @@ class CharacterBase:
         "elf": {"strength": 60, "max_HP": 100},
         "dwarf": {"strength": 120, "max_HP": 230},
     }
+
+    def __init__(self):
+        self._name = None
+        self._race = None
+        self._inventory = []
+        self._golds = random.randint(0, 100)
+
+        # combat attributes
+        self._strength = 0
+        self._max_HP = 0
+        self._current_HP = 0
 
 
 class Player(CharacterBase):
