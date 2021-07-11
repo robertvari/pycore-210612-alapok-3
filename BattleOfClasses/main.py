0dee4c2faa97bbe0f6265262b5446e4fdcc601da
Robert Vári <mail.robertvari@gmail.com>
2021-07-11 17:06:33 +0200

    battle of classes init

---
+++ BattleOfClasses/main.py
@@ -0,0 +1,10 @@
+class BattleOfClasses:
+    def __init__(self):
+        self._intro()
+
+    def _intro(self):
+        print("="*50, "BATTLE OF CLASSES", "="*50)
+
+
+if __name__ == '__main__':
+    BattleOfClasses()
